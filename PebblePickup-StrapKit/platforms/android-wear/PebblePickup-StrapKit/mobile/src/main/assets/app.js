be22e583f32118ab1ccef4bc99b46ea7946576ee
martinza <zackmartinsek@gmail.com>
2015-01-18 02:08:21 -0500

    Pebble StrapKit app

--- PebblePickup-StrapKit/platforms/android-wear/PebblePickup-StrapKit/mobile/src/main/assets/app.js
+++ PebblePickup-StrapKit/platforms/android-wear/PebblePickup-StrapKit/mobile/src/main/assets/app.js
@@ -1,13 +1,26 @@
 
 var introPage = StrapKit.UI.Page();
-
+introPage.setBackgroundColor('#42ff23');
 var introCard = StrapKit.UI.Card({
     title: 'Click Select',
     body: 'For Mad Game'
 });
 
-introCard.setOnClick(function() {
+var showLinePage = function(user, pickupline) {
+    var linePage = StrapKit.UI.Page();
+    linePage.setBackgroundColor('#42ff23');
+    var lineCard = StrapKit.UI.Card({
+        title: user,
+        body: pickupline
+    });
+    lineCard.setOnClick(refreshPickupLine);
+    linePage.addView(lineCard);
+    linePage.show();
+};
+
+var refreshPickupLine = function() {
     var loadingPage = StrapKit.UI.Page();
+    loadingPage.setBackgroundColor('#42ff23');
     var loadingText = StrapKit.UI.TextView({
         text: 'Chill I am Loading',
         position: 'center'
@@ -17,23 +30,21 @@ introCard.setOnClick(function() {
 
     StrapKit.HttpClient({
         url: 'https://www.kimonolabs.com/api/a38ll6me?apikey=drXUUfvK516nGaCNZwneGEIt6UZWtiWe',
-        type: 'json'   
+        type: 'json'
     }, function(data) {
-        var pickupline = data.results.collection1[0].pickupLine;
-        var user = data.results.collection1[0].user;
-
-        var linePage = StrapKit.UI.Page();
-        var lineCard = StrapKit.UI.Card({
-            title: user,
-            body: pickupline
-        });
-        linePage.addView(lineCard);
-        linePage.show();
+        var randLine = Math.floor(Math.random() * data.count);
+        console.log(randLine);
+        var pickupline = data.results.collection1[randLine].pickupLine;
+        var user = data.results.collection1[randLine].user;
+        console.log(pickupline);
+        showLinePage(user, pickupline);
+        loadingPage.hide();
     }, function(error) {
         console.log(error);
-    });
-});
+    }); 
+};
 
+introCard.setOnClick(refreshPickupLine);
 
 introPage.addView(introCard);
 introPage.show();
